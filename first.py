97cdee1fc016e074036a22166c33f6e95dac2bf1
ultrasuper <ultrasuper@outlook.com>
2015-11-19 20:28:49 +0800

    first commit

---
+++ first.py
@@ -0,0 +1,30 @@
+'''
+__author__ = "Sean"
+def func(name,*args,**kwargs):
+    print name
+    print len(args)
+    print len(kwargs)
+
+func("Sean",(1,2,3),count=3,sum=6)
+'''
+#!/usr/bin/env python
+
+import MySQLdb
+
+print "Content-Type: text/html\n"
+print "<html><head><title>Books</title></head>"
+print "<body>"
+print "<h1>Books</h1>"
+print "<ul>"
+
+connection = MySQLdb.connect(user='me', passwd='letmein', db='my_db')
+cursor = connection.cursor()
+cursor.execute("SELECT name FROM books ORDER BY pub_date DESC LIMIT 10")
+
+for row in cursor.fetchall():
+    print "<li>%s</li>" % row[0]
+
+print "</ul>"
+print "</body></html>"
+
+connection.close()
